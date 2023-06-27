72de52c4ba709cb3e53aa648ade39d9d5888dca3
Arsen Davtyan <Arsen.Davtyan@microchip.com>
2023-06-27 17:49:45 +0400

    Two components in Greeting

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-    return <h2>My First Component</h2>;
+    return (
+        <div>
+            <Person />
+            <Message />
+        </div>
+    );
+  }
+
+  const Person = () => <h2>john doe</h2>
+  const Message = () => {
+    return <h2>this is my message</h2>;
 }
+
+// function Greeting() {
+//     return React.createElement('h2', {}, 'hello world');
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
